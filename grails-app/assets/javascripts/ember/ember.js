644b539a6258b8cb8b299e0c400e09773b1f7ada
Sachin Deokar <deokar@outlook.com>
2016-03-29 20:05:42 -0400

    initial setup

---
+++ grails-app/assets/javascripts/ember/ember.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /ember/core/ember.core
+//= require /ember/index/ember.index
+
+angular.module("ember", [
+    "ember.core",
+    "ember.index"
+]);
